9901ee0ad074cd7b0d3004a9bfbcd5f323dde20f
Jay Dave <jaycdave@gmail.com>
2015-06-24 13:40:35 -0700

    added tableView and verified its working with some dummmy data

--- Patterns/ViewController.swift
+++ Patterns/ViewController.swift
@@ -8,18 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView! // adding the tableView to the page
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tableView.dataSource = self // required to set up the tableview
+        self.tableView.delegate = self // required to set up the tableview
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // function is asking us for how many rows do we want our table view to have, expecting an integer
+        return 5 // setting the rows to 5
     }
-
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // function is setting up what goes on each row        
+        var cell = UITableViewCell() // creating new varible for cell
+        cell.textLabel?.text = "Testing" // adding text to the cell
+        return cell // returning the cell
+        
+    }
 
 }
 
